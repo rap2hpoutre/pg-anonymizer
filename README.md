aa27c2c54687da6718461c8816c3785c7cd156c6
Raphaël Huchet <raph@selego.co>
2021-01-30 00:28:03 +0100

    init

---
+++ README.md
@@ -0,0 +1,32 @@
+pg-anonymizer
+=============
+
+Export your postgreSQL database anonymized
+
+[![oclif](https://img.shields.io/badge/cli-oclif-brightgreen.svg)](https://oclif.io)
+[![Version](https://img.shields.io/npm/v/pg-anonymizer.svg)](https://npmjs.org/package/pg-anonymizer)
+[![Downloads/week](https://img.shields.io/npm/dw/pg-anonymizer.svg)](https://npmjs.org/package/pg-anonymizer)
+[![License](https://img.shields.io/npm/l/pg-anonymizer.svg)](https://github.com/rap2hpoutre/pg-anonymizer/blob/master/package.json)
+
+<!-- toc -->
+* [Usage](#usage)
+* [Commands](#commands)
+<!-- tocstop -->
+# Usage
+<!-- usage -->
+```sh-session
+$ npm install -g pg-anonymizer
+$ pg-anonymizer COMMAND
+running command...
+$ pg-anonymizer (-v|--version|version)
+pg-anonymizer/0.1.0 darwin-x64 node-v14.15.3
+$ pg-anonymizer --help [COMMAND]
+USAGE
+  $ pg-anonymizer COMMAND
+...
+```
+<!-- usagestop -->
+# Commands
+<!-- commands -->
+
+<!-- commandsstop -->
---
+++ src/index.ts
@@ -0,0 +1,136 @@
+import { Command, flags } from "@oclif/command";
+import { promisify } from "util";
+import { exec } from "child_process";
+const faker = require("faker");
+const fs = require('fs');
+// import faker from 'faker';
+
+function dieAndLog(message: string, error: any) {
+  console.error(message);
+  console.error(error);
+  process.exit(1);
+}
+
+class PgAnonymizer extends Command {
+  static description = "dump anonymized database";
+
+  static args = [
+    {
+      name: "database",
+      description:
+        "database connection string, e.g: `postgresql://user:secret@localhost:1234/mybase`",
+      required: true,
+    },
+  ];
+
+  static flags = {
+    version: flags.version({ char: "v" }),
+    help: flags.help({ char: "h" }),
+    list: flags.string({
+      char: "l",
+      description: "list of columns to anonymize",
+      default:
+        "email,name,description,address,city,country,phone,comment,bithdate",
+    }),
+    output: flags.string({
+      char: "o",
+      description: "output file",
+      default: "output.sql",
+    }),
+    pgDumpOutputMemory: flags.string({
+      char: "m",
+      description: "max memory used to get output from pg_dump in MB",
+      default: "256",
+    }),
+  };
+
+  async originalDump(db: string, memory: number): Promise<string> {
+    const execPromisified = promisify(exec);
+    try {
+      console.log("Launching pg_dump");
+      const { stdout, stderr } = await execPromisified(`pg_dump ${db}`, {
+        maxBuffer: memory * 1024 * 1024,
+      });
+      if (stderr.trim()) {
+        dieAndLog("pg_dump command failed.", stderr);
+      }
+      return stdout;
+    } catch (e) {
+      dieAndLog("pg_dump command failed. Are you sure it is installed?", e);
+    }
+    return "";
+  }
+
+  async run() {
+    const { args, flags } = this.parse(PgAnonymizer);
+    const result = await this.originalDump(
+      args.database,
+      Number(flags.pgDumpOutputMemory)
+    );
+
+    let table = null;
+    let indices: Number[] = [];
+    let cols: string[] = [];
+
+    console.log("Command pg_dump done, starting anonymization.");
+    console.log("Output file: " + flags.output);
+    fs.writeFileSync(flags.output, '')
+
+    for (let line of result.split("\n")) {
+      if (line.match(/^COPY .* FROM stdin;$/)) {
+        table = line.replace(/^COPY (.*?) .*$/, "$1");
+        console.log("Anonymizing table " + table);
+
+        cols = line
+          .replace(/^COPY (?:.*?) \((.*)\)/, "$1")
+          .split(",")
+          .map((e) => e.trim())
+          .map((e) => e.replace('"', ""))
+          .map((e) => e.toLowerCase());
+
+        indices = cols.reduce((acc: Number[], value, key) => {
+          if (flags.list.includes(value)) acc.push(key);
+          return acc;
+        }, []);
+
+        if (indices.length)
+          console.log(
+            "Columns to anonymize: " +
+              cols.filter((v, k) => indices.includes(k)).join(", ")
+          );
+        else console.log("No columns to anonymize");
+      } else if (table && line.trim()) {
+        line = line
+          .split("\t")
+          .map((v, k) => {
+            if (indices.includes(k)) {
+              if (cols[k] === "email") return faker.internet.email();
+              if (cols[k] === "name") return faker.name.findName();
+              if (cols[k] === "description") return faker.random.words(3);
+              if (cols[k] === "address") return faker.address.streetAddress();
+              if (cols[k] === "city") return faker.address.city();
+              if (cols[k] === "country") return faker.address.country();
+              if (cols[k] === "phone") return faker.phone.phoneNumber();
+              if (cols[k] === "comment") return faker.random.words(3);
+              if (cols[k] === "bithdate") return faker.date.past();
+              return faker.random.word;
+            }
+            return v;
+          })
+          .join("\t");
+      } else {
+        table = null;
+        indices = [];
+        cols = [];
+      }
+      try {
+        fs.appendFileSync(flags.output, line + '\n');
+      } catch(e) {
+        dieAndLog('Failed to write file', e);
+      }
+      
+    }
+  }
+}
+
+export = PgAnonymizer;
