3bcd9de63428ba31a7cbeed1a30b988a9059adca
Jack Allen <38332643+jackall3n@users.noreply.github.com>
2023-03-20 19:37:36 +0100

    Add -skip and --preserve-null support. With chalk for logging. (#40)

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import { Command, flags } from "@oclif/command";
 import { spawn } from "child_process";
-const faker = require("faker");
-const fs = require("fs");
-const path = require("path");
-const readline = require("readline");
+import fs from 'fs';
+import path from 'path';
+import readline from 'readline';
 import { Input } from "@oclif/parser";
+import faker from 'faker';
+import chalk from 'chalk';
+import pluralize from 'pluralize';
 
 function dieAndLog(message: string, error: any) {
   console.error(message);
@@ -36,6 +38,11 @@ function sanitizePgDumpArgs(argv: string[]) {
   }
 }
 
+const log = {
+  info: (...args: unknown[]) => console.log(chalk.grey(...args)),
+  status: console.log
+}
+
 class PgAnonymizer extends Command {
   static description = "dump anonymized database";
 
@@ -58,7 +65,12 @@ class PgAnonymizer extends Command {
     help: flags.help({ char: "h" }),
     list: flags.string({
       char: "l",
-      description: "[default: email,name,description,address,city,country,phone,comment,birthdate] list of columns to anonymize",
+      description: "list of columns to anonymize",
+      default: "email,name,description,address,city,country,phone,comment,birthdate"
+    }),
+    skip: flags.string({
+      char: "s",
+      description: "specify tables to skip",
     }),
     configFile: flags.string({
       char: "c",
@@ -76,6 +88,11 @@ class PgAnonymizer extends Command {
     fakerLocale: flags.string({
       char: "f",
       description: "faker locale (e.g: en, fr, de)",
+    }),
+    'preserve-null': flags.boolean({
+      char: "n",
+      description: "preserve NULL values",
+      default: false
     }),
     pgDumpOutputMemory: flags.string({
       char: "m",
@@ -95,31 +112,18 @@ class PgAnonymizer extends Command {
       ? require(path.join(process.cwd(), flags.extension))
       : null;
 
-    sanitizePgDumpArgs(argv);
-    console.error("Launching pg_dump");
-    const pg = spawn("pg_dump", argv);
-    pg.on("exit", function (code) {
-      if (code != 0) {
-        dieAndLog("pg_dump command failed with exit code", code);
-      }
-    });
-    pg.stderr.on("data", function (data) {
-      dieAndLog("pg_dump command error:", data.toString());
-    });
-    pg.stdout.setEncoding("utf8");
-
     if (!(flags.list || flags.configFile)) {
       flags.list = "email,name,description,address,city,country,phone,comment,birthdate";
     }
 
-    let list: { col: string; replacement: string | null; }[];
+    let list: { col: string; replacement: string | null; }[] = [];
     if (flags.configFile) {
       list = fs.readFileSync(flags.configFile, "utf8")
         .split(/\r?\n/)
         .map((l: string) => l.trim())
         .map((l: string) => {
-          if (l === "") return null;
-          if (l.startsWith("#")) return null;
+          if (l === "") return null as never; // casting to never, as they're filtered out below
+          if (l.startsWith("#")) return null as never; // casting to never, as they're filtered out below
           return {
             col: l.replace(/:(?:.*)$/, "").toLowerCase(),
             replacement: l.includes(":") ? l.replace(/^(?:.*):/, "") : null
@@ -135,21 +139,51 @@ class PgAnonymizer extends Command {
       });
     }
 
+    log.info("List:", list.map(c => c.col).join(", "))
+
+    const skip = [];
+
+    if (flags.skip) {
+      const tables = flags.skip.split(",").map((t: string) => t.toLowerCase().trim())
+      skip.push(...tables)
+
+      log.info("Skipping:", tables.join(", "))
+    }
+
     let table: string | null = null;
     let indices: Number[] = [];
     let cols: string[] = [];
 
-    console.error("Command pg_dump started, running anonymization.");
-
     let out: any;
+
     if (flags.output === "-") {
       out = process.stdout;
       out._handle.setBlocking(true);
-      console.error("Output to stdout");
+      log.info("Output to stdout");
     } else {
       out = fs.createWriteStream(flags.output);
-      console.error("Output file: " + flags.output);
+      log.info("Output file: " + flags.output);
     }
+
+
+    sanitizePgDumpArgs(argv);
+
+    log.status()
+    log.status("Launching pg_dump...");
+
+    const pg = spawn("pg_dump", argv);
+
+    pg.on("exit", function (code) {
+      if (code != 0) {
+        dieAndLog("pg_dump command failed with exit code", code);
+      }
+    });
+    pg.stderr.on("data", function (data) {
+      dieAndLog("pg_dump command error:", data.toString());
+    });
+    pg.stdout.setEncoding("utf8");
+
+    log.status("Command pg_dump started, running anonymization.");
 
     const inputLineResults = readline.createInterface({
       input: pg.stdout,
@@ -158,8 +192,9 @@ class PgAnonymizer extends Command {
 
     for await (let line of inputLineResults) {
       if (line.match(/^COPY .* FROM stdin;$/)) {
+        log.info()
+
         table = line.replace(/^COPY (.*?) .*$/, "$1");
-        console.error("Anonymizing table " + table);
 
         cols = line
           .replace(/^COPY (?:.*?) \((.*)\).*$/, "$1")
@@ -175,59 +210,73 @@ class PgAnonymizer extends Command {
           return acc;
         }, []);
 
-        if (indices.length)
-          console.error(
-            "Columns to anonymize: " +
-              cols.filter((v, k) => indices.includes(k)).join(", ")
-          );
-        else console.error("No columns to anonymize");
+        log.status(chalk`{blueBright ${table}}:`, cols.map((c, i) => indices.includes(i) ? chalk.yellow(`[${c}]`) : c).join(", "))
+
+        if (skip.includes(table.toLowerCase())) {
+          log.status(`Skipping... excluded by user`);
+        } else if (!indices.length) {
+          log.status(`Skipping... no matching columns`)
+        } else {
+          log.status(`Anonymizing ${chalk.yellow(indices.length)} ${pluralize('column', indices.length)}...`);
+        }
       } else if (table && line.trim() && (line !== "\\.")) {
-        line = line
-          .split("\t")
-          .map((v, k) => {
-            if (indices.includes(k)) {
-              let replacement = list.find(
-                (l: any) => l.col === cols[k]
-              )?.replacement;
-              if (!replacement) {
-                replacement = list.find(
-                  (l: any) => l.col === table + "." + cols[k]
+        // Skip if specified, or there's no columns to anonymize
+        if (!skip.includes(table.toLowerCase()) && indices.length) {
+          line = line
+            .split("\t")
+            .map((v, k) => {
+              if (indices.includes(k)) {
+                if (flags['preserve-null'] && v === "\\N") {
+                  return v
+                }
+
+                let replacement = list.find(
+                  (l: any) => l.col === cols[k]
                 )?.replacement;
-              }
-              if (replacement) {
-                if (replacement.startsWith("faker.")) {
-                  const [_one, two, three] = replacement.split(".");
-                  if (!(two && three)) return replacement;
-                  if (two === "date")
-                    return postgreSQLDate(faker.date[three]());
-                  return faker[two][three]();
+                if (!replacement) {
+                  replacement = list.find(
+                    (l: any) => l.col === table + "." + cols[k]
+                  )?.replacement;
                 }
-                if (replacement.startsWith("extension.")) {
-                  const functionPath = replacement.split(".");
-                  return functionPath.reduce((acc: any, key: any) => {
-                    if (acc[key]) {
-                      return acc[key];
-                    }
-                    return acc;
-                  }, extension)(v, table);
+                if (replacement) {
+                  if (replacement.startsWith("faker.")) {
+                    const [, two, three] = replacement.split(".") as ["faker", keyof Faker.FakerStatic, string];
+                    if (!(two && three)) return replacement;
+
+                    const fn = (faker[two] as any)[three];
+
+                    if (two === "date") return postgreSQLDate(fn())
+
+                    return fn();
+                  }
+                  if (replacement.startsWith("extension.")) {
+                    const functionPath = replacement.split(".");
+                    return functionPath.reduce((acc: any, key: any) => {
+                      if (acc[key]) {
+                        return acc[key];
+                      }
+                      return acc;
+                    }, extension)(v, table);
+                  }
+                  return replacement;
                 }
-                return replacement;
+                if (cols[k] === "email") return faker.internet.email();
+                if (cols[k] === "name") return faker.name.findName();
+                if (cols[k] === "description") return faker.random.words(3);
+                if (cols[k] === "address") return faker.address.streetAddress();
+                if (cols[k] === "city") return faker.address.city();
+                if (cols[k] === "country") return faker.address.country();
+                if (cols[k] === "phone") return faker.phone.phoneNumber();
+                if (cols[k] === "comment") return faker.random.words(3);
+                if (cols[k] === "birthdate")
+                  return postgreSQLDate(faker.date.past());
+                return faker.random.word();
               }
-              if (cols[k] === "email") return faker.internet.email();
-              if (cols[k] === "name") return faker.name.findName();
-              if (cols[k] === "description") return faker.random.words(3);
-              if (cols[k] === "address") return faker.address.streetAddress();
-              if (cols[k] === "city") return faker.address.city();
-              if (cols[k] === "country") return faker.address.country();
-              if (cols[k] === "phone") return faker.phone.phoneNumber();
-              if (cols[k] === "comment") return faker.random.words(3);
-              if (cols[k] === "birthdate")
-                return postgreSQLDate(faker.date.past());
-              return faker.random.word();
-            }
-            return v;
-          })
-          .join("\t");
+
+              return v;
+            })
+            .join("\t");
+        }
       } else {
         table = null;
         indices = [];
